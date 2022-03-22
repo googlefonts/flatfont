a8537090ef3b2f3ac30ec58274ddb51ef8adc16e
Hamid Alian <hamidalian74@gmail.com>
2022-03-22 14:39:27 -0600

    formatting using Black

--- python/ufoff.py
+++ python/ufoff.py
@@ -1,42 +1,24 @@
-
 from ufoLib2 import Font
 from ufoLib2.converters import unstructure
 import flatbuffers
 from FlatFont.Ufo import FontInfo
 from FlatFont.Ufo import OpenTypeNameRecord
 from FlatFont.Ufo import OpenTypeGaspRangeRecord
-from enum import IntEnum
 
-SCALARS = [int, float, bool] # Other scalars needed # Enum needed
-NON_SCALARS = [str, list] # Other none-scalars needed
+SCALARS = [int, float, bool]  # Other scalars needed # Enum needed
+NON_SCALARS = [str, list]  # Other none-scalars needed
 
-def Capital(string: str): # seek equivalent buit-in function
-    '''Capitalizes the string without affecting other characters'''
+
+def capital(string: str):  # seek equivalent buit-in function
+    """Capitalizes the string without affecting other characters"""
     return string[0].upper() + string[1:]
 
-def StartAttrVector(attr: str):
-    '''
-    Simplifies calling buffer functions
-    Returns a string representing the buffer function 
-    Suitable for the "Getattr" function input
-    '''
-    return "Start" + Capital(attr) + "Vector"
 
-def AddAttr(attr: str): 
-    '''
-    Simplifies calling buffer functions
-    Returns a string representing the buffer function 
-    Suitable for the "Getattr" function input
-    '''
-    return "Add" + Capital(attr)
-
-def BuildAttrString(builder: flatbuffers.Builder, string: str):
-    '''Creates a buffer for strings'''
-    return builder.CreateString(string)
-
-def BuildAttrVector(builder: flatbuffers.Builder, attr: str ,vector: list, Value_Type: type = float):   
-    '''Creates a buffer for Vectors by passing the name of the attribute as a string'''
-    getattr(FontInfo, StartAttrVector(attr))(builder, len(vector)) # Fontinfo.StartAttrVector()
+def build_attr_vector(
+    builder: flatbuffers.Builder, attr: str, vector: list, Value_Type: type = float
+):
+    """Creates a buffer for Vectors by passing the name of the attribute as a string"""
+    getattr(FontInfo, "Start" + capital(attr) + "Vector")(builder, len(vector))
     for index in reversed(range(0, len(vector))):
         if Value_Type == float:
             builder.PrependFloat32(vector[index])
@@ -46,12 +28,13 @@ def BuildAttrVector(builder: flatbuffers.Builder, attr: str ,vector: list, Value
             builder.PrependBool(vector[index])
     return builder.EndVector()
 
-def BuildUfoBuffer(builder: flatbuffers.Builder, ufo: Font):
-    '''
+
+def build_ufo_buffer(builder: flatbuffers.Builder, ufo: Font):
+    """
     Creates a dict that contains references for each data type that needs the buffer
     Includes a for-loop to generate a reference for the buffer-needed fields
     To avoid NestedError
-    '''
+    """
     attr_dict = dict()
     # A for-loop to create a reference to all fields which needs the buffer to be created for avoiding NestedError
     info_dict = unstructure(ufo.info)
@@ -61,27 +44,38 @@ def BuildUfoBuffer(builder: flatbuffers.Builder, ufo: Font):
             attr_dict[attr] = info_dict[attr]
             continue
 
-        if type(info_dict[attr]) == str: 
-            attr_dict[attr] = BuildAttrString(builder, info_dict[attr])
+        if type(info_dict[attr]) == str:
+            attr_dict[attr] = builder.CreateString(info_dict[attr])
             continue
 
-        if type(info_dict[attr]) == list: # should I create reference for empty list?
+        if type(info_dict[attr]) == list:  # should I create reference for empty list?
             if not info_dict[attr]:
                 continue
 
-            if type(info_dict[attr][0]) in [int, float]: # have to discuss type conversions
-                attr_dict[attr] = BuildAttrVector(builder, attr, info_dict[attr])
+            if type(info_dict[attr][0]) in [
+                int,
+                float,
+            ]:  # have to discuss type conversions
+                attr_dict[attr] = build_attr_vector(builder, attr, info_dict[attr])
                 continue
-                
+
         if attr == "OpenTypeNameRecords":
             OpenTypeNameRecord_refs = []
             for index in range(len(info_dict[attr])):
-                OpenTypeNameRecord_string = builder.CreateString(info_dict[attr][index].string)
+                OpenTypeNameRecord_string = builder.CreateString(
+                    info_dict[attr][index].string
+                )
                 OpenTypeNameRecord.Start(builder)
                 OpenTypeNameRecord.AddNameID(builder, info_dict[attr][index].nameID)
-                OpenTypeNameRecord.AddPlatformID(builder, info_dict[attr][index].platformID)
-                OpenTypeNameRecord.AddEncodingID(builder, info_dict[attr][index].encodingID)
-                OpenTypeNameRecord.AddLanguageID(builder, info_dict[attr][index].languageID)
+                OpenTypeNameRecord.AddPlatformID(
+                    builder, info_dict[attr][index].platformID
+                )
+                OpenTypeNameRecord.AddEncodingID(
+                    builder, info_dict[attr][index].encodingID
+                )
+                OpenTypeNameRecord.AddLanguageID(
+                    builder, info_dict[attr][index].languageID
+                )
                 OpenTypeNameRecord.AddString(builder, OpenTypeNameRecord_string)
                 OpenTypeNameRecord_refs.append(OpenTypeNameRecord.End(builder))
 
@@ -94,43 +88,54 @@ def BuildUfoBuffer(builder: flatbuffers.Builder, ufo: Font):
         if attr == "openTypeGaspRangeRecords":
             openTypeGaspRangeRecord_refs = []
             for index in range(len(info_dict[attr])):
-                OpenTypeGaspRangeRecord.StartRangeGaspBehaviorVector(builder, len(info_dict[attr][index].rangeGaspBehavior))
+                OpenTypeGaspRangeRecord.StartRangeGaspBehaviorVector(
+                    builder, len(info_dict[attr][index].rangeGaspBehavior)
+                )
                 for j in reversed(range(info_dict[attr][index].rangeGaspBehavior)):
                     builder.PrependUint32(info_dict[attr][index].rangeGaspBehavior[j])
                 rangeGaspBehaviorVector = builder.EndVector()
                 OpenTypeGaspRangeRecord.start(builder)
-                OpenTypeGaspRangeRecord.AddRangeMaxPPEM(builder, info_dict[attr][index].rangeMaxPPEM)
-                OpenTypeGaspRangeRecord.AddRangeGaspBehavior(builder, rangeGaspBehaviorVector)
-                openTypeGaspRangeRecord_refs.append(OpenTypeGaspRangeRecord.End(builder))
-            
+                OpenTypeGaspRangeRecord.AddRangeMaxPPEM(
+                    builder, info_dict[attr][index].rangeMaxPPEM
+                )
+                OpenTypeGaspRangeRecord.AddRangeGaspBehavior(
+                    builder, rangeGaspBehaviorVector
+                )
+                openTypeGaspRangeRecord_refs.append(
+                    OpenTypeGaspRangeRecord.End(builder)
+                )
+
             FontInfo.StartOpenTypeGaspRangeRecordsVector(builder, len(info_dict[attr]))
             for index in reversed(range(len(info_dict[attr]))):
                 builder.PrependUOffsetTRelative(openTypeGaspRangeRecord_refs[index])
             attr_dict[attr] = builder.EndVector()
             continue
-    
-    '''Building the buffer with all scalar types and a reference for non-scalar types'''
-    FontInfo.Start(builder)    
+
+    """Building the buffer with all scalar types and a reference for non-scalar types"""
+    FontInfo.Start(builder)
     for attr in info_dict:
-        if not info_dict[attr]: continue
-        getattr(FontInfo, AddAttr(attr))(builder, attr_dict[attr])
+        if not info_dict[attr]:
+            continue
+        getattr(FontInfo, "Add" + capital(attr))(builder, attr_dict[attr])
     flat_ufo_info = FontInfo.End(builder)
     builder.Finish(flat_ufo_info)
-    return builder.Output()   
+    return builder.Output()
+
 
 def main():
-    '''
+    """
     Converts UFO (From UfoLib2.Font) to flatbuffer binary
     for JSON conversion, execute:
-    flatc --json --raw-binary ../schemas/ufo/fontinfo.fbs -- ufoff.bin    
-    '''
+    flatc --json --raw-binary ../schemas/ufo/fontinfo.fbs -- ufoff.bin
+    """
     ufo = Font.open("../../OswaldFont/legacy/3.0/Roman/400/src/Oswald--400.ufo")
     builder = flatbuffers.Builder(0)
 
     # Building the buffer by passing the references
-    buf = BuildUfoBuffer(builder, ufo)
+    buf = build_ufo_buffer(builder, ufo)
     with open("ufoff.bin", "wb") as outfile:
         outfile.write(buf)
+
 
 if __name__ == "__main__":
     main()
